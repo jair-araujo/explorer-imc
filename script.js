d07165c1ccc21379af701aefb859ddb18421283a
Jair Araujo <jair.jay@hotmail.com>
2022-10-01 15:05:17 -0300

    Refactored and created Modal structure

--- script.js
+++ script.js
@@ -3,9 +3,19 @@ const form = document.querySelector('form');
 const inputWeight = document.querySelector('#weight');
 const inputHeight = document.querySelector('#height');
 
-const modalWrapper = document.querySelector('.modal-wrapper');
-const modalMessage = document.querySelector('.modal .title span');
-const modalBtnClose = document.querySelector('.modal button.close');
+/* Anotação Object literals*/
+const Modal = {
+  wrapper: document.querySelector('.modal-wrapper'),
+  massage: document.querySelector('.modal .title span'),
+  buttonClose: document.querySelector('.modal button.close'),
+
+  open() {
+    Modal.wrapper.classList.add('open');
+  },
+  close() {
+    Modal.wrapper.classList.remove('open');
+  }
+};
 
 form.onsubmit = e => {
   e.preventDefault();
@@ -15,12 +25,12 @@ form.onsubmit = e => {
   const result = IMC(weight, height);
   const message = `Seu IMC é de ${result}`;
 
-  modalMessage.innerText = message;
-  modalWrapper.classList.add('open');
+  Modal.massage.innerText = message;
+  Modal.open();
 };
 
-modalBtnClose.onclick = () => {
-  modalWrapper.classList.remove('open');
+Modal.buttonClose.onclick = () => {
+  Modal.close();
   inputWeight.value = '';
   inputHeight.value = '';
 };
